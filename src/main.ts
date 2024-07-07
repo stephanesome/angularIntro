f6cb20d13d1ec8275dfa365c837eb155d514ef36
stephane <stephanesome@gmail.com>
2024-07-07 10:35:11 -0400

    Update to V18

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,6 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
-
-
-import { environment } from './environments/environment';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
-if (environment.production) {
-  enableProdMode();
-}
-
-bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule)]
-})
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
